225f20384ea8bc2c15b9ff760f7fe08f60d7fa3d
Jane <Frank@example.com>
2025-05-20 12:38:20 +0100

    added line

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         system.out.println(sayHelloToSomeone("Jane"));
+        
         System.out.println(sayGoodbye());
     }
 
